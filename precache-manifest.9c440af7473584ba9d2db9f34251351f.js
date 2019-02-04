542943e4dd48075829a099e2083d8f2b4180412a
Fahad Khan Ashrafi <mfahadkhanashrafi@outlook.com>
2019-02-04 21:56:27 +0500

    Updates

---
+++ precache-manifest.9c440af7473584ba9d2db9f34251351f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3ba79402af9e9433ee91",
+    "url": "/chat-app/static/js/runtime~main.3ba79402.js"
+  },
+  {
+    "revision": "5a218cf94f9b92c57ee9",
+    "url": "/chat-app/static/js/main.5a218cf9.chunk.js"
+  },
+  {
+    "revision": "bcaf9546396488280d02",
+    "url": "/chat-app/static/js/1.bcaf9546.chunk.js"
+  },
+  {
+    "revision": "5a218cf94f9b92c57ee9",
+    "url": "/chat-app/static/css/main.2093eb12.chunk.css"
+  },
+  {
+    "revision": "228d6bcfd0621aea3767bfcc5c734d60",
+    "url": "/chat-app/index.html"
+  }
+];
